a2d59b618d5a4af259636c10a43327a8a8a4daa9
David Lecomber <david@lecomber.net>
2026-04-29 23:53:06 +0100

    double-dash required to pass args to script

--- cron/run.sh
+++ cron/run.sh
@@ -3,7 +3,7 @@
 # Setup the cron job
 echo "$CRON_SCHEDULE cd /app && npm run snapshot" >> /etc/crontabs/root
 echo "$CRON_SCHEDULE_TIMELAPSE cd /app && npm run timelapse" >> /etc/crontabs/root
-echo "$CRON_SCHEDULE_PROGRESS cd /app && npm run timelapse --keep" >> /etc/crontabs/root
+echo "$CRON_SCHEDULE_PROGRESS cd /app && npm run timelapse -- --keep" >> /etc/crontabs/root
 
 crond -L /var/log/cron.log && tail -f /var/log/cron.log
 
